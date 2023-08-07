3fbe0b986ee3aa06cc1ad076015a0d48d7a54035
lol-chang <53506235+lol-chang@users.noreply.github.com>
2023-08-07 18:57:52 +0900

    feat : resister, login, DB update

---
+++ Likelion_Hackathon/src/main/java/com/diliter/digital_literacy/HomeController.java
@@ -0,0 +1,81 @@
+package com.diliter.digital_literacy;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import com.diliter.digital_literacy.MemberDto;
+
+import java.lang.reflect.Member;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    private final MemberService memberService;
+
+    @GetMapping("/")
+    public String index() {
+        return "main.html";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "Login.html";
+    }
+
+    @GetMapping("/resister")
+    public String saveForm() {
+        return "Resister.html";
+    }
+
+    @PostMapping("/resister")
+    public String save(@ModelAttribute MemberDto memberDto) {
+        memberService.save(memberDto);
+        return "Login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDto memberDto, HttpSession session) {
+
+        MemberDto loginResult = memberService.login(memberDto);
+
+        if (loginResult != null) {
+            session.setAttribute("loginResult", loginResult);
+
+
+//            loginResult.setMemberX(loginResult.getMemberX()+20);
+//            MemberEntity memberEntity = MemberEntity.toMemberEntity(loginResult);
+//            memberService.updateMember(memberEntity);
+
+            return "main.html";
+        }
+        else {
+            return "Login.html";
+        }
+    }
+
+
+
+//   클라이언트에서 클릭하면 서버로 통신
+
+    @GetMapping("/test")
+    public String testPage() {
+        return "test.html";
+    }
+
+    @PostMapping("/test")
+    @ResponseBody
+    public String receiveDataFromClient(@RequestBody String dataFromClient, HttpSession session) {
+        System.out.println("Data received from client: " + dataFromClient);
+        MemberDto loginResult = (MemberDto) session.getAttribute("loginResult"); // 세션에서 MemberDto를 가져옴
+        System.out.println("_______LoginResult : " + loginResult);
+        if (loginResult != null) {
+            loginResult.setMemberX(loginResult.getMemberX() + 20);
+            MemberEntity memberEntity = MemberEntity.toMemberEntity(loginResult);
+            memberService.updateMember(memberEntity);
+        }
+        return "Data received";
+    }
+
+
+}
